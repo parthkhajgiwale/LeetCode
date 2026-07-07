ab0bdcd32b83b6e6cb9033c116b74203eda7e785
Parth Khajgiwale <90266917+parthkhajgiwale@users.noreply.github.com>
2026-07-07 16:21:36 +0530

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,18 +1,29 @@
-def is_valid(s):
+def isValid(s):
     stack = []
-    mappings = {
-        '}' : '{',
-        ']' : '[',
-        ')' : '('
-    }
-    
-    for char in s:
-        if char in mappings:
-            top = stack.pop() if stack else '#'
-            if top != mappings[char]:
-                return False
-        else:
-            stack.append(char)
-    return not stack
 
-print(is_valid('(){}'))
+    matching = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+
+    for char in s:
+        # If opening bracket, push it
+        if char in "({[":
+            stack.append(char)
+
+        # If closing bracket, check the top of stack
+        else:
+            if not stack or stack[-1] != matching[char]:
+                return False
+
+            stack.pop()
+
+    # Stack should be empty if all brackets matched
+    return len(stack) == 0
+
+
+# Examples
+print(isValid("({[]})"))  # True
+print(isValid("([)]"))    # False
+print(isValid("((())"))   # False
